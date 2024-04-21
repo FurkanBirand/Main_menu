4a5bfe82814c1a47c25fe5872d91d1f5205a3cd4
FurkanBirand <furkan.gormez@wseiz.edu.pl>
2024-04-21 20:15:24 +0200

    Main menu
    Main menu

--- Main_Menu.py
+++ Main_Menu.py
@@ -4,8 +4,8 @@ import button
 
 
 pygame.init()
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -14,13 +14,19 @@ pygame.display.set_caption("Main Menu")
 resume_img = pygame.image.load("button_resume.png").convert_alpha()
 options_img = pygame.image.load("button_options.png").convert_alpha()
 quit_img = pygame.image.load("button_quit.png").convert_alpha()
-
+video_img = pygame.image.load("button_video.png").convert_alpha()
+keys_img = pygame.image.load("button_keys.png").convert_alpha()
+back_img = pygame.image.load("button_back.png").convert_alpha()
+audio_img = pygame.image.load("button_audio.png").convert_alpha()
 
 
 
 resume_button=button.Button(304,125,resume_img,(1))
 options_button=button.Button(304,245,options_img,(1))
 quit_button=button.Button(304,365,quit_img,(1))
+
+
+
 #game variables
 game_paused = False
 
@@ -36,7 +42,8 @@ def draw_text(text, font,text_col,x,y):
 current=True
 while current:
 
-    screen.fill((52,78,91))
+    background=pygame.image.load("summer.png").convert_alpha()
+    screen.blit(background,(0,0))
 
     #check if game is paused
     if game_paused ==True:
