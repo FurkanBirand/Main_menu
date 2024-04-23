95f2b46f47f645ab0fce47ebed593bc337c44784
FurkanBirand <furkan.gormez@wseiz.edu.pl>
2024-04-23 22:32:10 +0200

    some updates
    some updates

--- Main_Menu.py
+++ Main_Menu.py
@@ -25,10 +25,14 @@ resume_button=button.Button(304,125,resume_img,(1))
 options_button=button.Button(304,245,options_img,(1))
 quit_button=button.Button(304,365,quit_img,(1))
 
-
+audio_button=button.Button(304,125,audio_img,(1))
+keys_button=button.Button(304,245,keys_img,(1))
+back_button=button.Button(304,365,back_img,(1))
+video_button=button.Button(304,5,video_img,(1))
 
 #game variables
 game_paused = False
+menu_state= "main"
 
 font = pygame.font.SysFont("arialblack", 40)
 #define color
@@ -47,12 +51,24 @@ while current:
 
     #check if game is paused
     if game_paused ==True:
+       #check menu state
+     if menu_state == "main":
        if  resume_button.draw(screen):
            game_paused = False
-       elif options_button.draw(screen):
-           game_paused = False
+       if options_button.draw(screen):
+            menu_state = "options"
        if quit_button.draw(screen):
            current = False
+     if menu_state == "options":
+         if video_button.draw(screen):
+             print("video settings")
+         if audio_button.draw(screen):
+             print("audio settings")
+         if keys_button.draw(screen):
+             print("change keys settings")
+         if back_button.draw(screen):
+             menu_state = "main"
+
     else:
         draw_text("Press SPACE", font, TEXT_COL,160,250)
 
